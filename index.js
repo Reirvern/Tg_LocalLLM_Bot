99a724ab4a3d5f9241221680fb7e322a53f9cc52
Reirvern <maximbachevskiy@gmail.com>
2024-09-10 14:40:58 +0300

    Добавлено содержимое проекта

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const {Bot} = require('grammy');
+
+const bot = new Bot(process.env.BOT_API_KEY)
+
+bot.command('start', async (ctx) => {
+    await ctx.reply('...System activate...')
+})
+
+bot.start();
+
+ 
